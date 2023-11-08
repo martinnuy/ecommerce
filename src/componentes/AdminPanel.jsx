bc09cdaee6db92bae9260765559f5815e8170168
Martin <martinsebastian1314@hotmail.com>
2023-11-07 22:56:18 -0300

    UserTable panel

--- src/componentes/AdminPanel.jsx
+++ src/componentes/AdminPanel.jsx
@@ -1,62 +1,75 @@
-import React, { useState } from 'react'
-import NavSimple from './NavSimple'
-import PanelButton from './PanelButton'
-import '../hojas-de-estilos/AdminPanel.css';
-import AgregarProducto from './AgregarProducto';
-import MostrarProductos from './MostrarProductos';
-import Subtitulo from './Subtitulo';
-import AgregarCategoria from './AgregarCategoria';
+import React, { useState } from "react";
+import NavSimple from "./NavSimple";
+import PanelButton from "./PanelButton";
+import "../hojas-de-estilos/AdminPanel.css";
+import AgregarProducto from "./AgregarProducto";
+import MostrarProductos from "./MostrarProductos";
+import Subtitulo from "./Subtitulo";
+import AgregarCategoria from "./AgregarCategoria";
+import UsersTabla from "./UsersTabla";
 
 function AdminPanel() {
+  const [ventana, setVentana] = useState(<AgregarProducto />);
 
-  const [ventana, setVentana] = useState(<AgregarProducto/>);
-
-  const logoutFunction = ()=>{
-      localStorage.removeItem('token');
-      window.location.reload();
-  }
+  const logoutFunction = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
   return (
     <div>
-        <NavSimple />
-      
+      <NavSimple />
+
       <div className="container-fluid">
         <div className="row">
           <nav className="col-md-2 d-none d-md-block bg-dark sidebar fixed-top vh-100 z-index-nav">
             <div className="position-sticky div-link-panel">
               <ul className="nav flex-column">
-                <PanelButton tittle="Inicio"/>
-                <PanelButton tittle="Pedidos"/>
-                <PanelButton tittle="Estadisticas"/>
-                
-                <PanelButton tittle="Productos" onclick={ ()=>{
-                  setVentana(
-                    <div className='div-principal'>
-                      <Subtitulo titulo="Todos los Productos"/>
-                      <MostrarProductos categoria=""/>
-                    </div>
-                  )
-                  } }/>
-                
-                <PanelButton tittle="Agregar Producto" onclick={ ()=>{setVentana(<AgregarProducto/>)} }/>
-                <PanelButton tittle="Categorias" onclick={ ()=>{setVentana(<AgregarCategoria/>)} }/>
-                <PanelButton tittle="Usuarios"/>
-                <PanelButton tittle="Ofertas"/>
-                <PanelButton tittle="Cerrar Sesion" onclick={logoutFunction}/>
+                <PanelButton tittle="Inicio" />
+                <PanelButton tittle="Pedidos" />
+                <PanelButton tittle="Estadisticas" />
+
+                <PanelButton
+                  tittle="Productos"
+                  onclick={() => {
+                    setVentana(
+                      <div className="div-principal">
+                        <Subtitulo titulo="Todos los Productos" />
+                        <MostrarProductos categoria="" />
+                      </div>
+                    );
+                  }}
+                />
+
+                <PanelButton
+                  tittle="Agregar Producto"
+                  onclick={() => {
+                    setVentana(<AgregarProducto />);
+                  }}
+                />
+                <PanelButton
+                  tittle="Categorias"
+                  onclick={() => {
+                    setVentana(<AgregarCategoria />);
+                  }}
+                />
+                <PanelButton
+                  tittle="Usuarios"
+                  onclick={() => {
+                    setVentana(<UsersTabla />);
+                  }}
+                />
+                <PanelButton tittle="Ofertas" />
+                <PanelButton tittle="Cerrar Sesion" onclick={logoutFunction} />
               </ul>
             </div>
           </nav>
-          <div className='col-md-2'></div>
-          <main className="col-md-10">
-            
-            {ventana}
-
-          </main>
+          <div className="col-md-2"></div>
+          <main className="col-md-10">{ventana}</main>
         </div>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default AdminPanel
+export default AdminPanel;
---
+++ src/componentes/UsersTabla.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Subtitulo from "./Subtitulo";
+import { useQuery } from 'react-query';
+import LoadSpinner from "./LoadSpinner";
+import { format } from 'date-fns';
+
+
+function UsersTabla() {
+
+
+    const { data: traerUsuarios, isLoading, isError } = useQuery(
+      'userData', // Clave única para la consulta
+      async () => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          throw new Error('No se proporcionó un token válido.');
+        }
+  
+        const response = await fetch(`${process.env.REACT_APP_API_URI}/users`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos');
+        }
+  
+        const data = await response.json();
+        return data;
+      },
+      {
+        staleTime: 60000, // Establece un período de caché de 60 segundos
+      }
+    );
+  
+  
+
+  if (isLoading) {
+    return (
+      <LoadSpinner />
+      )
+  }
+
+  if (isError) {
+    return <div className="div-principal">Error al obtener los datos</div>;
+  }
+
+  return (
+
+    
+    <div className="div-principal">
+      <Subtitulo titulo="Informacion de Usuarios" />
+      <table className="table table-light table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col">Verificado</th>
+            <th scope="col">Registro</th>
+            <th scope="col">Ultimo Login</th>
+            <th scope="col">Nº Compras</th>
+            <th scope="col">Rol</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        {traerUsuarios.map((p, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{ p.username }</td>
+            <td>{ p.email }</td>
+            <td>{ p.verificado ? 'Si' : 'No' }</td>
+            <td>{ format(new Date(p.createdAt), 'dd/MM/yyyy - HH:mm') }</td>
+            <td>{ format(new Date(p.lastLogin), 'dd/MM/yyyy - HH:mm') }</td>
+            <td>{ p.numeroDeCompras }</td>
+            <td>{ p.role }</td>
+          </tr>
+        ))}
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UsersTabla;
